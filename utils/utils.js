735a52a905df5d13e63c5a49ffcb09cc2a40d7fd
Салахутдинов Тимур Ренатович <tsalakhutdinov@kzn.beeline.ru>
2025-03-25 20:38:15 +0300

    update utils

--- utils/utils.js
+++ utils/utils.js
@@ -1,27 +1,25 @@
 const mysql = require('mysql2/promise');
 
-const urlRegex = /^https?:\/\/(www\.)?[a-zA-Z\0-9]+\.[\w\-._~:/?#[\]@!$&'()*+,;=]{2,}#?$/;
+const urlRegex =
+	/^https?:\/\/(www\.)?[a-zA-Z\0-9]+\.[\w\-._~:/?#[\]@!$&'()*+,;=]{2,}#?$/;
 
 const emailRegex = /^[a-z0-9-]+@[a-z0-9-.]+/i;
 
-const allowedCors = [
-  'localhost:3000',
-  'http://tatbayar.ru',
-];
+const allowedCors = ['localhost:3002', 'http://tatbayar.ru'];
 
 const pool = mysql.createPool({
-  host: '127.0.0.1',
-  user: 'root',
-  password: 'timur2003',
-  database: 'bayar',
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
+	host: 'localhost',
+	user: 'bayar_user',
+	password: 'Bayar@1234',
+	database: 'bayar',
+	waitForConnections: true,
+	connectionLimit: 10,
+	queueLimit: 0,
 });
 
 module.exports = {
-  emailRegex,
-  urlRegex,
-  allowedCors,
-  pool,
+	emailRegex,
+	urlRegex,
+	allowedCors,
+	pool,
 };
